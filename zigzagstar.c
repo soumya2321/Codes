6aa0189bc0bf7de0ae1810249a9adeb43b9308fb
Soumya <sahukarsoumya6@gmail.com>
2025-09-06 14:59:09 +0530

    zigzagstar.c

--- zigzagstar.c
+++ zigzagstar.c
@@ -35,3 +35,34 @@ for(r=1;r<=repeat;r++)
 return 0;
 }
 
+another form 
+#include <stdio.h>
+int main()
+{
+int n,k,j,i,m=5;
+scanf("%d",&n);
+for(k=1;k<=n;k++)
+{
+    for(i=1;i<=m;i++)
+    {
+      for(j=0;j<=i-1;j++)
+      {
+        printf(" ");
+      }
+      printf("*****\n");
+    }   
+ 
+    for(i=m-2;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf(" ");
+            
+        }
+        printf("*****\n");
+    }
+    
+}
+return 0;
+}
+
